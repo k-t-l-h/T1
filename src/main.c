77d579b5d2c3e13855b46d96296401870be95ed1
k-t-l-h <laciedreamer@gmail.com>
2021-03-15 01:28:25 +0300

    safety

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define SIZE 32
@@ -18,8 +19,7 @@ typedef struct {
     char Country[SIZE];
 } Toy;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     //ошибка: файл с датой не передан или не передано название
     if (argc < 3) {
         printf("Not enough arguments\n");
@@ -36,29 +36,34 @@ int main(int argc, char** argv)
     int check = 0;
 
     check = fscanf(fp, "%d", &n);
-    if (check != 1 )
-        {
-            printf("Incorrect data format\n");
+    if (check != 1) {
+        printf("Incorrect data format\n");
+        return 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+
+        Toy *toy = (Toy*)malloc(sizeof(Toy));
+        if (NULL == toy) {
+            printf("Allocation problem\n");
             return 0;
         }
 
+        //EILSEQ, EINVAL and other check
+        //SHOULD specify a field width for the "%s" string placeholder
+        fscanf(fp, "%31s", &toy->Name);
+        fscanf(fp, "%f", &toy->Price);
+        fscanf(fp, "%f", &toy->Avaliable);
+        fscanf(fp, "%31s", &toy->Country);
 
-
-
-    for (int i = 0; i < n; i++) {
-        Toy toy;
-        //TODO: обработка ошибок чтения
-        fscanf(fp, "%s", &toy.Name);
-        fscanf(fp, "%f", &toy.Price);
-        fscanf(fp, "%f", &toy.Avaliable);
-        fscanf(fp, "%s", &toy.Country);
-
-        if (strcmp(toy.Country, argv[2]) == 0) {
-            printf("Name: %s\n", toy.Name);
-            printf("Price: %f\n", toy.Price);
-            printf("Avaliable: %d\n", toy.Avaliable);
-            printf("Country: %s\n", toy.Country);
+        if (strcmp(toy->Country, argv[2]) == 0) {
+            printf("Name: %s\n", toy->Name);
+            printf("Price: %f\n", toy->Price);
+            printf("Avaliable: %d\n", toy->Avaliable);
+            printf("Country: %s\n", toy->Country);
         }
+
+        free(toy);
     }
 
     //Не забываем закрыть файл
