757d07b11c9c3449d0907e70660cb5ebf10c30d0
k-t-l-h <laciedreamer@gmail.com>
2021-03-25 22:24:10 +0300

    one more fix lib installation

--- libs/naive/test_check.cpp
+++ libs/naive/test_check.cpp
@@ -4,6 +4,10 @@ extern "C" {
     #include "check.h"
 }
 
+int predicate(int a) {
+    return 1;
+}
+
 //не передан предикат
 TEST(TEST_CHECK, no_predicate) {
     size_t num = 0;
@@ -14,16 +18,16 @@ TEST(TEST_CHECK, no_predicate) {
 //не передан массив
 TEST(TEST_CHECK, no_array) {
     size_t num = 0;
-    num = check([](int l) {return true;}, nullptr, 0);
+    num = check(predicate, nullptr, 0);
     ASSERT_EQ(num, 0);
 }
 
 //не передан массив
 TEST(TEST_CHECK, all_good) {
-    int[] arr = [1, 2, 3, 4];
-    size_t num = 0;
-    num = check([](int l) {return true;}, nullptr, 4);
-    ASSERT_EQ(num, 4);
+    int arr[5] = {};
+    size_t num = 5;
+    num = check(predicate, arr, num);
+    ASSERT_EQ(num, num);
 }
 
 int main(int argc, char** argv) {
