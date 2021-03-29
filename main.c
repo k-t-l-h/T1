9db0079ea6c2dae44428a92606743cfe4b552b03
k-t-l-h <laciedreamer@gmail.com>
2021-03-29 03:43:33 +0300

    stress test lib name fix

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include "libs/naive/check.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -21,7 +20,7 @@ int* read_from_file(char* filename, size_t num) {
     FILE* fp = NULL;
     fp = fopen(filename, "r");
     if (NULL == fp) {
-        printf("file not found");
+        printf("file not found\n");
         return NULL;
     }
 
@@ -29,7 +28,7 @@ int* read_from_file(char* filename, size_t num) {
     //считать из файла
     for (size_t i = 0; i < num; ++i) {
         if (fscanf(fp, "%d", &arr[i]) != 1) {
-            printf("not enough nums!");
+            printf("not enough nums!\n");
             free(arr);
             fclose(fp);
             return NULL;
@@ -63,7 +62,7 @@ int main(int argc, char** argv) {
             case 'n':
                 num = atoi(optarg);
                 if (num <= 0) {
-                    printf("Incorrect number of numbers");
+                    printf("Incorrect number of numbers\n");
                     return -1;
                   }
                 break;
@@ -80,23 +79,21 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    clock_t start_t, end_t, total_t;
+    clock_t start_t, end_t;
     //наивная реализация
     start_t = clock();
     size_t result = 0;
-    printf("code: %d, result: %zu", check(predicate, arr, num, &result), result);
+    printf("code: %d, result: %zu\n", check(predicate, arr, num, &result), result);
     end_t = clock();
-    total_t = (end_t - start_t) / CLOCKS_PER_SEC;
-    printf("Time taken: %ld", total_t);
+    double total_t = (double) (end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Time taken: %lf\n", total_t);
 
     //работа с динамической библиотекой
-
-
     void *library;
-    library = dlopen("checks.so", RTLD_NOW);
+    library = dlopen("./libPREDICATE_CHECK_P.so", RTLD_NOW);
     if (NULL == library) {
       free(arr);
-      printf("Библиотека checks.so была не найдена, вернитесь позже");
+      printf("Библиотека ./libPREDICATE_CHECK_P.so была не найдена, вернитесь позже");
       return -1;
     }
     //создаем указатель на функцию
@@ -105,10 +102,10 @@ int main(int argc, char** argv) {
 
     //прогоняем тесты
     start_t = clock();
-    printf("code: %d, result: %zu", check_p(predicate, arr, num, &result), result);
+    printf("code: %d, result: %zu\n", check_p(predicate, arr, num, &result), result);
     end_t = clock();
-    total_t = (end_t - start_t) / CLOCKS_PER_SEC;
-    printf("Время, затраченное параллельной реализацией: %ld", total_t);
+    total_t = (double) (end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Время, затраченное параллельной реализацией: %lf", total_t);
 
     dlclose(library);
 
